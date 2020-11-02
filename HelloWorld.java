3ef5b2bec48d160118392c824852f063f0c9de37
Ravi <rchellaboina@gmail.com>
2020-11-02 15:07:40 +0530

    Restored the file from second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,8 @@
 // Your First Program
-//This Program will output Hello World
 
 class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-//	Prints "Welcome to GIT" in the Terminal Window
-        System.out.println("Welcome GIt!"); 
     }
 }
